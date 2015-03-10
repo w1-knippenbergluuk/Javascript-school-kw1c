95d757242ab551ab7837a402f7c5a6dc3705d55f
w1-knippenbergluuk <luuk.knippenberg@edu-kw1c.nl>
2015-03-10 10:48:02 +0100

    Nieuw begin opdracht 412

--- Javascipt/Hoofdstuk 4/Opdracht 412/_js/javascript.js
+++ Javascipt/Hoofdstuk 4/Opdracht 412/_js/javascript.js
@@ -11,14 +11,16 @@ vervoer = $('input[name=vervoer]:checked').val();
 
 //Dit is de functie die wordt aangeroepen als je op wijzig klikt
 function wijzigSnelheid() {
+    
 // De eerste stap is dat de waardes van de globale variabelen
  // snelheid, vervoer en weg worden gevuld met de ingevulde/gekozen waardes in het formulier
 getWaardes();
+alert('test');
 alert('plek: ' + weg);
 alert('vervoer: ' + vervoer);
 alert('snelheid: ' + snelheid);
 /* Schrijf hieronder je code */
-    alert('test');
+    
 }
 
 //Met deze functie kun je een melding in de pagina schrijven.( in de div met id=’status’ )
