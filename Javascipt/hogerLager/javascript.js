85c9b96837a6eef0563aa95aa809fd91acaa358d
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2015-03-13 18:09:23 +0100

    HOGER / LAGER opdracht bijna af?

--- Javascipt/hogerLager/javascript.js
+++ Javascipt/hogerLager/javascript.js
@@ -1,20 +1,27 @@
     var randomGetal = Math.floor((Math.random() * 1000) + 1);
+    var pogingen = [];
+    var uitslag = 1000;
+    console.log(randomGetal);
 
-   function bereken(){
-        var getalElement = document.getElementById('input');
-        var input = getalElement.value;
-        
-        if(input < randomGetal){
-            alert('Je zit te laag!')
-        }   else    {
-            if(input > randomGetal){
-                alert('Je zit te hoog!')
-            }   else{
-                if(input == randomGetal){
-                    alert('U heeft het goed!')
-                }
-            }
+function bereken(){
+    var getalElement = document.getElementById('input');
+    var input = getalElement.value;
+    pogingen[pogingen.length] = "1";
+    
+    if(input < randomGetal){
+        document.getElementById('voortgang').innerHTML = 'Poging ' + pogingen.length + ' - Je zit te laag!';
+    }   else    {
+        if(input > randomGetal){
+            document.getElementById('voortgang').innerHTML = 'Poging ' + pogingen.length + ' - Je zit te hoog!';
+        }   else{
+                document.getElementById('voortgang').innerHTML = 'Poging ' + pogingen.length + ' - Je hebt het geraden!';
+                berekenScore();
         }
+    }
+    
+}
 
-        alert(randomGetal);
+function berekenScore(){
+    var uitslag = 1000 - (pogingen.length * 50);
+    document.getElementById('scoreUitslag').innerHTML += '<h1>' + uitslag + '</h1>';
    }
