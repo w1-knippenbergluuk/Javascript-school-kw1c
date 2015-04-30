42827151970b57810832213f82eb808a3a074153
Luuk Knippenberg <luuk_knippenberg@hotmail.com>
2015-04-30 09:33:21 +0200

    eventsopdracht halverwege

--- Javascipt/Knip/EventsOefening/events.js
+++ Javascipt/Knip/EventsOefening/events.js
@@ -1,9 +1,28 @@
 $(document).ready(function () {
 
+    $(this).toggleClass('overheen');
 
+    $('#lijst li.overheen').mouseenter(function () {
+        $(this).toggleClass('overheen');
+        //.fadeOut(10000);
+    });
+
+    $('#lijst li.overheen').mouseleave(function () {
+        $(this).toggleClass('overheen');
+    });
 
     $('#opdrachten li').click(function () {
         $(this).toggleClass('gedaan');
         //.fadeOut(10000);
+    });
+
+    $('.waardering').click(function () {
+        var stemmen = parseInt($(this).text()) + 1;
+        $(this).text(stemmen);
+    });
+
+    $('.waardering').dblclick(function () {
+        var stemmen = parseInt($(this).text()) - 3;
+        $(this).text(stemmen);
     });
 })
